480987ae3d84a5212bc54213e53858a1c290dd2b
ojayWillow <og.vitols@gmail.com>
2026-01-18 17:16:22 +0200

    Fix: Add ButtonLink to UI exports

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,6 @@
 // Export all UI components from a single file for easier imports
 export { Button } from './Button';
+export { ButtonLink } from './ButtonLink';
 export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card';
 export { Container } from './Container';
 export { Section } from './Section';
